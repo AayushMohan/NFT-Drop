c1ec400d02d6a21683f59087d3638d7375476e94
AayushMohan <aayushmohan1702@gmail.com>
2022-04-06 22:01:17 +0530

    Added Right Section

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -14,7 +14,7 @@ const NFTDropPage = () => {
             />
           </div>
           <div className="space-y-2 p-5 text-center">
-            <h1 className="text-4xl font-bold text-white">Bored Apes</h1>
+            <h1 className="text-4xl font-bold text-white">BORED APES</h1>
             <h2 className="text-xl text-gray-300">
               A Collection of Bored Apes who live & breathe React!
             </h2>
@@ -23,7 +23,42 @@ const NFTDropPage = () => {
       </div>
 
       {/* Right */}
-      <div></div>
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+        {/* Header */}
+        <header className="flex items-center justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              BORED APES
+            </span>{' '}
+            NFT Market Place
+          </h1>
+
+          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
+            Sign In
+          </button>
+        </header>
+
+        <hr className="my-2 border" />
+
+        {/* Content */}
+
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="https://links.papareact.com/bdy"
+            alt=""
+          />
+
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+            The BORED APES Coding Club | NFT Drop
+          </h1>
+
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+        </div>
+
+        {/* Mint Button */}
+      </div>
     </div>
   )
 }
